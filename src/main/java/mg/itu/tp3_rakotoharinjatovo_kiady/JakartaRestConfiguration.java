cb1a00ca48b5c4eddee17b8f0a4bf7faa1bc0bee
kiady11 <kiady.rakotoharinjatovo@yahoo.com>
2023-03-26 18:26:32 +0300

    Création du projet via le TP2

---
+++ src/main/java/mg/itu/tp3_rakotoharinjatovo_kiady/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package mg.itu.tp3_rakotoharinjatovo_kiady;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/mg/itu/tp3_rakotoharinjatovo_kiady/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tp3_rakotoharinjatovo_kiady.config;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+/**
+ *
+ * @author kiady
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+
+}
